53ed998f1368d06e07a723f87d28075b3eee1491
Andrei Maris <xarenwo@gmail.com>
2020-04-19 00:23:49 +0200

    Create pamac_install.sh
    This script installs pamac gui for aur repository on a fresh arch installation.

---
+++ pamac_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# install pamac from aur
+sudo pacman -S --needed base-devel git wget yajl \
+&& cd /tmp \
+&& git clone https://aur.archlinux.org/package-query.git \
+&& cd package-query/ \
+&& makepkg -si && cd /tmp/ \
+&& git clone https://aur.archlinux.org/yaourt.git \
+&& cd yaourt/ \
+&& makepkg -si \
+&& yaourt -S pamac-aur
